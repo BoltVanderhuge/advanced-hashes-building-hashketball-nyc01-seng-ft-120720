12d0bc05047904602725ebce33fa3afa1c82be9e
John Wisneski <john.j.wisneski@gmail.com>
2020-11-28 21:06:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,10 +4,10 @@ def game_hash
   {
     team_name = "Brooklyn Nets",
     colors = ["Black", "White"],
-    players ={
-      player_name = "Alan Anderson",
+    players =[
+      {player_name = "Alan Anderson",
       
-    }
+    ]
     
   }
   binding.pry
